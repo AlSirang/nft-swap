b86383abdffc601301d8e14e65b7cc1d572f060f
Bashir Uddin <uddinbashir1999@gmail.com>
2023-08-20 17:39:55 +0500

    fix: add logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,15 +5,27 @@ export const Header = () => {
   return (
     <header className="max-w-7xl m-auto px-5 py-3">
       <div className="flex justify-between">
-        <Link href="/" className="text-xl font-semibold">
-          Swap NFT
+        <Link href="/">
+          <picture>
+            <img
+              src="/icons/logo-white.png"
+              alt="logo"
+              className="max-h-[60px]"
+            />
+          </picture>
         </Link>
 
         <nav className="flex gap-10 items-center">
-          <Link href="/explore" className="text-lg">
+          <Link
+            href="/explore"
+            className="text-2xl font-semibold hover:text-slate-300 transition-all"
+          >
             Explore
           </Link>
-          <Link href="/profile" className="text-lg">
+          <Link
+            href="/profile"
+            className="text-2xl font-semibold hover:text-slate-300 transition-all"
+          >
             Profile
           </Link>
 
