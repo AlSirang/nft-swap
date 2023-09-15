13b084986ade0cd1574593536178d15c8c2f5e82
Bashir Uddin <uddinbashir1999@gmail.com>
2023-09-15 23:48:20 +0500

    fix: update prod endpoint

--- frontend/src/configs/axios.config.ts
+++ frontend/src/configs/axios.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "http://localhost"
+    ? "https://nft-swap-sigma.vercel.app/api"
     : "http://localhost:3000/api";
 
 export const httpClient = axios.create({
